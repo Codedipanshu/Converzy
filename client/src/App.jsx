8cc09c35a7bb660e37b0b40b8fb9a197d898f1de
Dipanshu Agrawal <codedipanshu@gmail.com>
2025-04-13 17:34:28 +0530

    footer added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import Home from "./pages/Home";
 
 function App() {
-    return <Home />;
+  return (
+    <>
+      <Home />
+      <p className=" text-center">Made with ❤️ by Dipanshu</p>
+    </>
+  );
 }
 
 export default App;
